3c6d59cfff99dd1c368aacd79f3b9f6a94e97d8a
alan alcantara <89042656+aln8575@users.noreply.github.com>
2024-10-08 19:52:51 -0300

    site quase pronto

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,11 @@
-
-
 const myObserver = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show')
-        } else {
-            entry.target.classList.remove('show')
-        }
-    })
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show')
+    } else {
+      entry.target.classList.remove('show')
+    }
+  })
 })
 
 const elements = document.querySelectorAll('.hidden')
@@ -16,19 +14,64 @@ elements.forEach((element) => myObserver.observe(element))
 
 
 function openModal(modalId) {
-    document.getElementById(modalId).style.display = 'flex';
+  document.getElementById(modalId).style.display = 'flex';
+}
+
+function closeModal(modalId) {
+  document.getElementById(modalId).style.display = 'none';
+}
+
+
+function openCustomModal(modalId) {
+  document.getElementById(modalId).style.display = 'flex';
+}
+
+// Função para fechar o novo modal
+function closeCustomModal(modalId) {
+  document.getElementById(modalId).style.display = 'none';
+}
+
+class MobileNavbar {
+  constructor(mobileMenu, navList, navLinks) {
+    this.mobileMenu = document.querySelector(mobileMenu);
+    this.navList = document.querySelector(navList);
+    this.navLinks = document.querySelectorAll(navLinks);
+    this.activeClass = "active";
+
+    this.handleClick = this.handleClick.bind(this);
   }
-  
-  function closeModal(modalId) {
-    document.getElementById(modalId).style.display = 'none';
-  }   
-  
-  
-  function openCustomModal(modalId) {
-      document.getElementById(modalId).style.display = 'flex';
+
+  animateLinks() {
+    this.navLinks.forEach((link, index) => {
+      link.style.animation
+        ? (link.style.animation = "")
+        : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+            index / 7 + 0.3
+          }s`);
+    });
+  }
+
+  handleClick() {
+    this.navList.classList.toggle(this.activeClass);
+    this.mobileMenu.classList.toggle(this.activeClass);
+    this.animateLinks();
+  }
+
+  addClickEvent() {
+    this.mobileMenu.addEventListener("click", this.handleClick);
+  }
+
+  init() {
+    if (this.mobileMenu) {
+      this.addClickEvent();
     }
-  
-    // Função para fechar o novo modal
-    function closeCustomModal(modalId) {
-      document.getElementById(modalId).style.display = 'none';
-    }
+    return this;
+  }
+}
+
+const mobileNavbar = new MobileNavbar(
+  ".mobile-menu",
+  ".nav-list",
+  ".nav-list li",
+);
+mobileNavbar.init();
