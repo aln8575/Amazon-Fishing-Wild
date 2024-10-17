0309100a3c01f87da851bb270b9afcdc13bf8f5a
alan alcantara <89042656+aln8575@users.noreply.github.com>
2024-10-17 10:02:53 -0300

    ok

--- js/multSpeciesModal.js
+++ js/multSpeciesModal.js
@@ -15,3 +15,17 @@ function openModal(modalId) {
     function closeCustomModal(modalId) {
       document.getElementById(modalId).style.display = 'none';
     }
+
+
+// Selecionar a logo
+const logo = document.querySelector('.logo');
+
+// Função que verifica o scroll da página
+window.addEventListener('scroll', function() {
+  // Se a página for rolada mais que 100px, a logo some
+  if (window.scrollY > 790) {
+    logo.classList.add('logo-hidden');
+  } else {
+    logo.classList.remove('logo-hidden');
+  }
+});
