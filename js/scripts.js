8c633816825c170db1f90e127bfd37b4aeb7df16
alan alcantara <89042656+aln8575@users.noreply.github.com>
2024-10-23 16:00:32 -0300

    alteracoes ok

--- js/scripts.js
+++ js/scripts.js
@@ -75,3 +75,20 @@ const mobileNavbar = new MobileNavbar(
   ".nav-list li",
 );
 mobileNavbar.init();
+
+
+/*=============== SHOW MENU ===============*/
+const showMenu = (toggleId, navId) =>{
+   const toggle = document.getElementById(toggleId),
+         nav = document.getElementById(navId)
+
+   toggle.addEventListener('click', () =>{
+       // Add show-menu class to nav menu
+       nav.classList.toggle('show-menu')
+
+       // Add show-icon to show and hide the menu icon
+       toggle.classList.toggle('show-icon')
+   })
+}
+
+showMenu('nav-toggle','nav-menu')
