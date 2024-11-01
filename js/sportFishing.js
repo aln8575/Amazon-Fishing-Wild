4002284e229472e2db9e2d0dfe8480f5eb9e3962
alan alcantara <89042656+aln8575@users.noreply.github.com>
2024-11-01 11:11:33 -0300

    folder

--- js/sportFishing.js
+++ js/sportFishing.js
@@ -15,3 +15,9 @@ function openModal(modalId) {
     function closeCustomModal(modalId) {
       document.getElementById(modalId).style.display = 'none';
     }
+
+    document.querySelectorAll('.close').forEach(button => {
+      button.addEventListener('click', () => {
+          closeModal('modal-4');
+      });
+  });
